f1d1c145092c61387dd97e863b4195b61fc651f0
Nedzib Sastoque <nedzib.sastoque@gmail.com>
2025-03-07 17:43:44 -0500

    feat: algunos cambios aislados y el uso de avante con codeium

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>cc", function()
+  local relative_path = vim.fn.fnamemodify(vim.fn.expand("%"), ":~:.")
+  vim.fn.setreg("+", relative_path)
+  vim.notify("Ruta relativa copiada: " .. relative_path)
+end, { desc = "Copiar ruta relativa del archivo" })
+
